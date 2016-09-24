c7014045f3205bb92039e0f629a582a32b906360
274737258 <weixin@weixindeMacBook-Air.local>
2016-09-24 13:20:47 +0800

    init

---
+++ wkzf/wkzf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wkzf
+//
+//  Created by 魏欣 on 16/9/24.
+//  Copyright © 2016年 魏欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wkzf/wkzf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wkzf
+//
+//  Created by 魏欣 on 16/9/24.
+//  Copyright © 2016年 魏欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
